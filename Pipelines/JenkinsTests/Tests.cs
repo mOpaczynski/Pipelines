0342b0f61c1875f917c37a7c63bfab4ef49095cf
mopaczynski <mopaczynski@future-processing.com>
2018-02-13 12:44:33 +0100

    write first basic test

---
+++ Pipelines/JenkinsTests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace JenkinsTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            int number = 10;
+
+            Assert.IsTrue(number == 10);
+        }
+    }
+}
