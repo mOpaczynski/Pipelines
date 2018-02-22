b8804db08d4f7fb0865da8673ac078b3bb515da2
mopaczynski <mopaczynski@future-processing.com>
2018-02-22 19:04:37 +0100

    code cleanup, generate new assemblies

--- Pipelines/CakeExtensions/CakeExtensions.cs
+++ Pipelines/CakeExtensions/CakeExtensions.cs
@@ -15,40 +15,25 @@ namespace CakeExtensions
         [CakeMethodAlias]
         public static void MigrateDatabases(this ICakeContext context, string projectConfiguration = "Release", string migrationConfiguration = "Configuration", int? targetMigration = null)
         {
-            context.Log.Information("hello");
-            var root = GetRootPath();
-            context.Log.Information(Invariant($"root: {root.FullName}"));
-
-            context.Log.Information(Invariant($"searching for migrate.exe"));
-            if (!Directory.Exists(root.FullName))
-            {
-                context.Log.Information(Invariant($"root directory not exists"));
-            }
-            var migrateExecFile = Directory.GetFiles(Invariant($"{root.FullName}"), "migrate.exe", SearchOption.AllDirectories).First();
-            context.Log.Information($"migrateExecFile: {migrateExecFile}");
-            var dataAccessDirectories = Directory.GetDirectories(root.FullName, "*.DataAccess", SearchOption.AllDirectories);
-            context.Log.Information($"dataAccessDirectories: {dataAccessDirectories}");
+            var solutionRootDirectory = GetRootPath();
+            var migrateExecFile = Directory.GetFiles(Invariant($"{solutionRootDirectory.FullName}"), "migrate.exe", SearchOption.AllDirectories).First();
+            var dataAccessDirectories = Directory.GetDirectories(solutionRootDirectory.FullName, "*.DataAccess", SearchOption.AllDirectories);
 
             foreach (var dataAccessDirectory in dataAccessDirectories)
             {
-                context.Log.Information($"Data acc dir: {dataAccessDirectory}");
                 var configFilePath = Directory.GetFiles(dataAccessDirectory, "App.config", SearchOption.TopDirectoryOnly).First();
-                context.Log.Information($"configFilePath: {configFilePath}");
-                var assembyFilePath = Directory.GetFiles(dataAccessDirectory, $"{GetLastSegment(dataAccessDirectory)}.dll", SearchOption.AllDirectories).First(x => x.Contains("bin") && x.Contains($"{projectConfiguration}"));
-                context.Log.Information($"assembyFilePath: {assembyFilePath}");
-                var migrateExecFileCopyPath = $"{Path.GetDirectoryName(assembyFilePath)}/migrate.exe";
-                context.Log.Information($"migrateExecFileCopyPath: {migrateExecFileCopyPath}");
-                var arguments = $"{GetLastSegment(dataAccessDirectory)}.dll {migrationConfiguration} /startupConfigurationFile:\"{configFilePath}\" /targetMigration:\"{targetMigration}\" /verbose";
-                context.Log.Information($"arguments: {arguments}");
+                var assembyFilePath = Directory.GetFiles(dataAccessDirectory, Invariant($"{GetLastSegment(dataAccessDirectory)}.dll"), SearchOption.AllDirectories).First(x => x.Contains("bin") && x.Contains(projectConfiguration));
+                var migrateExecFileCopyPath = Invariant($"{Path.GetDirectoryName(assembyFilePath)}/migrate.exe");
+                var migrateExecArguments = Invariant($"{GetLastSegment(dataAccessDirectory)}.dll {migrationConfiguration} /startupConfigurationFile:\"{configFilePath}\" /targetMigration:\"{targetMigration}\" /verbose");
 
                 if (!File.Exists(migrateExecFileCopyPath))
                 {
-                    File.Copy(migrateExecFile, migrateExecFileCopyPath, true);
+                    File.Copy(migrateExecFile, migrateExecFileCopyPath);
                 }
 
                 Process runMigrator = new Process();
                 runMigrator.StartInfo.FileName = migrateExecFileCopyPath;
-                runMigrator.StartInfo.Arguments = arguments;
+                runMigrator.StartInfo.Arguments = migrateExecArguments;
                 runMigrator.StartInfo.UseShellExecute = false;
                 runMigrator.StartInfo.RedirectStandardOutput = true;
                 runMigrator.StartInfo.RedirectStandardError = true;
@@ -62,7 +47,7 @@ namespace CakeExtensions
 
                 if (runMigrator.ExitCode != 0)
                 {
-                    throw new Exception($"Migrate.exe: Process returned an error (exit code {runMigrator.ExitCode}).");
+                    throw new Exception(Invariant($"Migrate.exe: Process returned an error (exit code {runMigrator.ExitCode})."));
                 }
             }
         }
